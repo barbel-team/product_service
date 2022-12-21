42c7d6394c5548c3b8cdc135f92ac442ba253b0e
Identityless <rlandwnd555@naver.com>
2022-12-22 04:33:47 +0900

    feat : 전화번호로 주문 찾는 커스텀 쿼리 추가

--- src/main/java/com/barbel/product_service/repository/OrderRepository.java
+++ src/main/java/com/barbel/product_service/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.barbel.product_service.repository;
 import com.barbel.product_service.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Optional<List<Order>> findOrdersByRes_tel(String tel);
 }
